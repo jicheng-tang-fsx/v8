bd98ae833ba14ab27a8de48109f6cbf2b4e2e588
jicheng-tang-fsx <jicheng.tang@finstadiumx.co.jp>
2024-04-09 17:04:23 +0900

    add draw

--- draw.py
+++ draw.py
@@ -6,7 +6,8 @@ import sys
 
 def generate_and_save_histogram(csv_file_path):
     """
-    读取CSV文件，绘制耗时的直方图，并保存为同名的JPG文件。
+    读取CSV文件，绘制耗时的直方图，并以高分辨率保存为同名的JPG文件。
+    直方图的bins以数量1为尺度，更密集。图形布局将被优化以增加精确性。
 
     参数:
     csv_file_path: str，CSV文件的路径。
@@ -14,18 +15,26 @@ def generate_and_save_histogram(csv_file_path):
     # 载入CSV文件
     data = pd.read_csv(csv_file_path)
 
+    # 计算bins的数量，基于最大值和最小值的差，加1确保覆盖所有可能的值
+    min_value = data['CostMillisecond'].min()
+    max_value = data['CostMillisecond'].max()
+    bins = int(max_value - min_value + 1)
+
     # 使用Seaborn绘制直方图
     plt.figure(figsize=(10, 6))
-    sns.histplot(data['CostMillisecond'], kde=True, color='skyblue', bins=30, alpha=0.7)
+    sns.histplot(data['CostMillisecond'], kde=False, color='skyblue', bins=bins, alpha=0.7)
     plt.title('Cost Millisecond Distribution')
     plt.xlabel('Cost Millisecond')
     plt.ylabel('Frequency')
 
+    # 优化图形布局
+    plt.tight_layout()
+
     # 构建保存文件的路径，将.csv扩展名替换为.jpg
     save_file_path = os.path.splitext(csv_file_path)[0] + '.jpg'
 
-    # 保存图表为JPG文件
-    plt.savefig(save_file_path)
+    # 保存图表为JPG文件，提高分辨率
+    plt.savefig(save_file_path, dpi=300)
 
     # 清除当前图形
     plt.close()
